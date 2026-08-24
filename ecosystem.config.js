9eaf4ee3a2186eee7ee48b3b4d98936ea8b39e55
dev-adt <dev5.adtgroup@gmail.com>
2026-08-24 09:33:42 +0700

    change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'tanhoangnga',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3000',
+      args: 'start -p 3022',
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true,
@@ -11,7 +11,7 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 3022
       }
     }
   ]
